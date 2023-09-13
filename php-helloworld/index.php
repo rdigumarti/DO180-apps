325c4a03255a39eec6be4ee04e600356c3518fa7
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2023-09-13 14:42:10 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
 print "Hello, Team Infosys....! php version is " . PHP_VERSION . "\n";
+print "Hello, Team Infosys....! php version is " . PHP_VERSION . "\n";
 ?>
