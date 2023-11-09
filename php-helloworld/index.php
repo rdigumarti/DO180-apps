7fcf71585f58a923ac1760a1753cef4fc22093f9
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2023-11-09 16:01:06 +0530

    Update index.php
    deleted 2 lines oc code

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,6 +1,4 @@
 <?php
 print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
 print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
-print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
-print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
 ?>
