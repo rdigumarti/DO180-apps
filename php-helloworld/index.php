633fe4f5b5e2d5dd7532139ad581fea8c98492e6
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2023-11-09 15:57:26 +0530

    Update index.php
    added 3 lines

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
 print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
+print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
+print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
+print "Hello everone .... Welcome to the session" . PHP_VERSION . "\n";
 ?>
