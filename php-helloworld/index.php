378959a0e75e3105206df0f4251047368bb674c9
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2024-10-23 13:54:43 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
 print "Hello everone .... Welcome Red Hat Training DO288######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training DO288######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training DO288######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training DO288######" .   PHP_VERSION . "\n";
 ?>
