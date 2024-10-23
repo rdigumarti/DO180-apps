05468f76d876dd0e0a6b704573e7f5cfedd7140c
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2024-10-23 09:48:13 +0530

    Update index.php
    added 2 lined

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,5 @@
 <?php
 print "Hello everone .... Welcome Red Hat Virtal Training DO288" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Virtal Training DO288" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Virtal Training DO288" .   PHP_VERSION . "\n";
 ?>
