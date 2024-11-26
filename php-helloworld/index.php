74332051d067b790efa7bb9711056ec6a124550d
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2024-11-26 11:34:33 +0530

    Update index.php to DO180

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello everone .... Welcome Red Hat Training DO288######" .   PHP_VERSION . "\n";
-print "Hello everone .... Welcome Red Hat Training DO288######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
 ?>
