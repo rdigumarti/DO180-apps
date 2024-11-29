9d5089e60fac0b8ddde479c74b381f5e9f331426
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2024-11-29 11:55:35 +0530

    Update index.phpsfdksd

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -3,6 +3,5 @@ print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION .
 print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
 print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION . "\n";
 print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
-print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION . "\n";
-print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
+
 ?>
