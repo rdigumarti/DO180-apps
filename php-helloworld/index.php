4bd8240f381ac1029aab8a7a6cbfcfc927da122e
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2024-11-29 11:49:11 +0530

    Update index.php with more lines

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,8 @@
 <?php
 print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION . "\n";
 print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training D180######" .   PHP_VERSION . "\n";
+print "Hello everone .... Welcome Red Hat Training DO180######" .   PHP_VERSION . "\n";
 ?>
