c9ad4fd907cc05931648cce23b2b37978fdbf096
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-02-18 14:27:45 +0530

    Update index.php
    new change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
 print "Hello team APAC.... Welcome Red Hat Training D)288######" .   PHP_VERSION . "\n";
-print "Hello team APAC.... Welcome Red Hat Training D)288######" .   PHP_VERSION . "\n";
+print "Hello everyone...! This is new change".   PHP_VERSION . "\n";
 ?>
