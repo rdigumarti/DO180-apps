c7f845684f280e204ff54e7d9fa271de7177521c
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-03-27 12:40:50 +0530

    Update index.php
    updated lines

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,6 @@
 <?php
 print "Hello Team Reliance Jio ...!".   PHP_VERSION . "\n";
+print "Hello Team Reliance Jio ...!".   PHP_VERSION . "\n";
+print "Hello Team Reliance Jio ...!".   PHP_VERSION . "\n";
+print "Hello Team Reliance Jio ...!".   PHP_VERSION . "\n";
 ?>
