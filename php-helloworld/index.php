f22c52e66ba6c520b42f0dc578de0b54450f207d
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-06-16 14:29:09 +0530

    Update index.php
    updated lines

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,3 @@
 <?php
-print "Hello Team Siemens... ...!".   PHP_VERSION . "\n";
-print "Hello Team Siemens... ...!".   PHP_VERSION . "\n";
-print "Hello Team Siemens... ...!".   PHP_VERSION . "\n";
+print "Hello Team APAC... ...!".   PHP_VERSION . "\n";
 ?>
