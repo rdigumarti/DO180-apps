75af3802d163d0b6d17bbe66d97eb3680be8fef9
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-06-25 12:45:20 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello Team Micron... ...!".   PHP_VERSION . "\n";
 print "Hello Team Micron... ...!".   PHP_VERSION . "\n";
-print "Hello Team Micron... ...!".   PHP_VERSION . "\n";
 ?>
