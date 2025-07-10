5b7a349f5b11e9dea78612d91c86495a8471f948
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-07-10 15:33:28 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -4,4 +4,7 @@ print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
 print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
 print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
 print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
+print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
+print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
+print "Hello Team APAC.. ...!".   PHP_VERSION . "\n";
 ?>
