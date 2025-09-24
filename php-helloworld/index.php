49ed0861c7ac9b9d2b4cb3080cbf73b9894eb0a5
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-09-24 12:46:47 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -2,4 +2,7 @@
 print "Hello Team.. welcome to Red Hat Training.. ...!".   PHP_VERSION . "\n";
 print "Hello Team.. welcome to Red Hat Training.. ...!".   PHP_VERSION . "\n";
 print "Hello Team.. welcome to Red Hat Training.. ...!".   PHP_VERSION . "\n";
+print "Hello Team.. welcome to Red Hat Training.. ...!".   PHP_VERSION . "\n";
+print "Hello Team.. welcome to Red Hat Training.. ...!".   PHP_VERSION . "\n";
+print "Hello Team.. welcome to Red Hat Training.. ...!".   PHP_VERSION . "\n";
 ?>
