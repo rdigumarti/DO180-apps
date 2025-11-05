78f53ac7ba3fb2bea22ec6985ae7a4bf3259e064
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-11-05 14:45:56 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,5 @@
 <?php
 print "Hi Christopher.. hope you enjoyed learning OpenShift.. ...!".   PHP_VERSION . "\n";
+print "Hi Christopher.. hope you enjoyed learning OpenShift.. ...!".   PHP_VERSION . "\n";
 
 ?>
