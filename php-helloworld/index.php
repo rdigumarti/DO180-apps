c565433ba1f9c0b76d3c564d673bdc5b3c0f86d7
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2025-11-26 09:16:08 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Thank you all for attending the Training.. ...!".   PHP_VERSION . "\n";
+print "Thank you all for attending the Training.. New change updated...!".   PHP_VERSION . "\n";
 ?>
