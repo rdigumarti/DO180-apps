e47a95b4ed2d976a6d7cc63124b723fdfc4f8bc9
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2026-08-25 12:50:06 +0530

    updated

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
 print "Hello, Team APAC ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
+print "Hello, Team APAC ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
 ?>
