f3aee41657df533816cabf889a8949c56456ece5
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2026-09-14 12:42:23 +0530

    Update greeting message for Team TCS

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,4 @@
 <?php
-print "Hello, Team APAC ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
-print "Hello, Team APAC ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
+print "Hello, Team TCS ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
+print "Hello, Team TCS -(IT) ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
 ?>
