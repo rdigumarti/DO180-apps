59a7d4aca0bbdf0c23640a79ec71bdfe18aa7095
rdigumarti <79555822+rdigumarti@users.noreply.github.com>
2026-09-17 15:24:52 +0530

    Remove redundant greeting message

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,3 @@
 <?php
 print "Hello, Team TCS ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
-print "Hello, Team TCS -(IT) ..Welcome to Red Hat Training.. ....... " . PHP_VERSION . "\n";
 ?>
